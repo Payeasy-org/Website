3145969c2531d36af7ae6fc782930385a5323f1b
Jessica <jessicaezike.je@gmail.com>
2025-05-15 16:38:30 +0100

    Adjusted image size on feat branch

--- app/forms/retailer/page.tsx
+++ app/forms/retailer/page.tsx
@@ -4,16 +4,13 @@ import Image from "next/image";
 export default function RetailerForm() {
     return (
         <div className="flex flex-row items-center bg-white gap[57px]">
-  <Image
-    src="/Start.png"
-    alt="Retailer app image"
-    width={936}
-    height={936}
-    className="object-contain mr-[57px]"
-  />
-
-
-            {/* <Image src="/Start.png" alt="App on phone" width={700} height={700} className="absolute top-[125px] -left-[225px] z-0"/> */}
+            <Image
+                src="/Start.png"
+                alt="Retailer app image"
+                width={936}
+                height={936}
+                className="object-contain mr-[57px]"
+            />
             <div className="flex1 w-[644px] h[766px] mt-[59px] p-[40px] bg-[rgba(206,152,252,0.2)] rounded-[20px]">
                 <div className="flex flex-col gap-[44px]">
                     <h2 className="font-semibold text-[40px] leading-[100%] tracking-[-0.01em] text-black">Let's Bring Convenience <br /> to your Customers</h2>
